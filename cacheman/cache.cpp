795a8ae5fabbe5ae39c61188af12384595a63fdc
Vedaant Arya (v3d3) <vedaant.arya.v3d3@gmail.com>
2021-03-24 17:06:03 +0530

    Implemented Memory, CacheBlock. Moving to Set.

--- cacheman/cache.cpp
+++ cacheman/cache.cpp
@@ -5,6 +5,14 @@
 *    Question : CS2610 A6
 -------------------------------------------------------------------------------------------------*/
 
+/*
+    progress (+ implemented ~ current - left)
+        + memory
+        + block
+        ~ set
+        - cache
+*/
+
 typedef unsigned int uint;
 
 // Cache Replacement Policies
@@ -21,100 +29,183 @@ typedef unsigned int uint;
 #define C_HIN 1
 
 /*-------------------------------------------------------------------------------------------------
+*    Class Name         : Memory
+*    Application        : Simulates memory
+*    Inheritances       : Nil
+-------------------------------------------------------------------------------------------------*/
+
+class Memory
+{
+public:
+    void read(uint addr, uint* buffer, uint wordCount = 1);
+    void write(uint addr, uint* buffer, uint wordCount = 1);
+};
+
+void Memory::read(uint addr, uint* buffer, uint wordCount = 1)  {
+    //dummy memory, does nothing
+    for(uint i = 0; i < wordCount; i++)  {
+        buffer[i] = 0;
+    }
+}
+
+void Memory::write(uint addr, uint* buffer, uint wordCount = 1)  {
+    //does absolutely nothing
+}
+
+/*-------------------------------------------------------------------------------------------------
 *    Class Name         : CacheBlock
 *    Application        : Used to represent a block of data in a cache
 *    Inheritances       : Nil
 -------------------------------------------------------------------------------------------------*/
-typedef struct cacheblockst
+class CacheBlock
 {
-    uint*       tag = NULL;       //tag of a cache block
-    bool        dirty = false;    //a bit to indicate whether any changes are made to a block or not.
-    bool        valid = false;    //valid bit
-    cacheblock* nextPtr = NULL;   //pointer to a cache block.
-    uint*       data = NULL;      //stores data of the cacheBlock
-    int         blockSize;
+private:
+    int    tag = -1;         //tag of a cache block
+    bool   dirty = false;    //a bit to indicate whether any changes are made to a block or not.
+    bool   valid = false;    //valid bit
+    
+    int    blockSize;
+    uint*  data = NULL;      //stores data of the cacheBlock
 
-    cacheblock(int blockSize)    //constructor for the cacheblock
+public:
+    CacheBlock(int blockSize)
     {
         this->blockSize = blockSize;    //getting values into struct variables
-        this->data  = new uint[blockSize];   //the data of cache according to block
+        this->data      = new uint[blockSize];   //the data of cache according to block
     }
+    
+    bool isValid();
+    bool isDirty();
+    
+    uint getTag();
+    void setTag(uint tag);
 
-    void  write(uint* data);
-    uint* read();
+    void write(uint offset, uint* data, uint count = 1);
+    void read(uint offset, uint* bufferWord, uint count = 1);
+};
 
-} cacheblock;
+typedef struct BlockNodest
+{
+    CacheBlock*          block = NULL;
+    struct BlockNodest*  next  = NULL;
+}  BlockNode;
 
+bool CacheBlock::isValid()  {
+    return valid;
+}
+
+bool CacheBlock::isDirty()  {
+    return dirty;
+}
+
+void CacheBlock::write(uint offset, uint* data, uint count = 1)  {
+    assert(valid);
+    assert(count < blockSize);
+    assert(offset + count < blockSize);
+
+    dirty = true;
+    
+    for(int i = 0; i < count; i++)  {
+        this->data[offset + i] = data[i];
+    }
+}
+
+void CacheBlock::read(uint offset, uint* data, uint count = 1)  {
+    assert(valid);
+    assert(count < blockSize);
+    assert(offset + count < blockSize);
+    
+    for(int i = 0; i < count; i++)  {
+        data[i] = this->data[offset + i];
+    }
+}
 
 /*-------------------------------------------------------------------------------------------------
-*    Class Name         : set
+*    Class Name         : Set
 *    Application        : Used to represent a set of blocks
 *    Inheritances       : Nil
 -------------------------------------------------------------------------------------------------*/
-class set
+class Set
 {
 private:
-    cacheblock* setRoot;    //head pointer of linked list of cache blocks
-    int setSize;
-    int repPolicy; //setSize is number of blocks in the cache, repPolocy is to identify replcement policy
-    int blockSize;
-    void blockReplacement();
+    BlockNode*  head;       //head pointer of linked list of cache blocks
+    int         size;       //size is number of blocks in the cache
+    int         repPolicy;  //repPolicy is to identify replcement policy
+    int         blockSize;
+
+    void        blockReplacement();
+    uint        getTag(uint addr);
 
 public: //publicFunctions
-    set(int setSize, int repPolicy, int blockSize)
+    Set(int setSize, int repPolicy, int blockSize)
     {
-        this->setSize = setSize;    //storing arguments into private variables of set
+        //absorb params
+        this->size = setSize;
         this->repPolicy = repPolicy;
         this->blockSize = blockSize;    
 
-        this->setRoot = new cacheblock(/*enter Args*/); 
-        cacheblock* temp = this->setRoot;
+        //allocate blocks
+        this->head = new BlockNode(); 
+        this->head->block = new CacheBlock(blockSize);
 
-        for(int i=1; i<setSize; i++)
+        BlockNode* temp = this->head;
+
+        for(int i = 1; i < setSize; i++)
         {
-            temp->nextPtr = new cacheblock(/*enter Args*/); //creating next cacheBlock
-            temp = temp->nextPtr;
+            temp->next = new BlockNode();
+            temp->next->block = new CacheBlock(blockSize);
+
+            temp = temp->next;
         }
+
+        //at this point, all blocks have been allocated, and are marked as invalid (default)
     }
 
-    void setRead(); //reading a data from a paticular set
-    void setWrite();    //writing the data to a particular set
+    //read from an address
+    uint read(uint address);
+    void write(uint address, uint data);
 };
-
 
 /*-------------------------------------------------------------------------------------------------
 *    Class Name         : Cache
 *    Application        : Used to represent the cache memory
 *    Inheritances       : Nil
 -------------------------------------------------------------------------------------------------*/
-class cache
+class Cache
 {
 private:
-    int setCount;   //number of sets in the cache
-    int cacheSize;  //size of the cache
-    int blockSize;  //size of the cache block
+    int numSets;   //number of sets in the cache
     int numBlocks;  //number of blocks in the cache
     int numWays;    //number of ways in the cache
+
+    int cacheSize;  //size of the cache
+    int blockSize;  //size of the cache block
+    
     int repPolicy;  //replacement policy
-    set* cacheSet;  //pointer to represent sets
+    Set** sets;  //pointer to represent sets
 
 public: //public functions
-    cache(int cacheSize, int blockSize, int org, int repPolicy)
+    Cache(int cacheSize, int blockSize, int org, int repPolicy)
     {
         //absorb params
         this->cacheSize = cacheSize;
         this->blockSize = blockSize;
-        this->numBlocks = cacheSize/blockSize;
+
+        this->numBlocks = cacheSize / blockSize;
         this->repPolicy = repPolicy;
 
         //set associativity
         if(org == 0)  {
             org = this->numBlocks;
         }
-        this->setCount = (this->numBlocks) / org;
+        this->numSets = (this->numBlocks) / org;
 
         //allocating required memory for sets
-        cacheSet = new set[setCount]((this->numBlocks)/(this->setCount), repPolicy, blockSize);
+        sets = new Set*[numSets];
+        for(int i = 0; i < numSets; i++)
+        {
+            sets[i] = new Set((this->numBlocks)/(this->numSets), repPolicy, blockSize);
+        }
     }
 
     int read(int);      //reading from a required adress in cache
